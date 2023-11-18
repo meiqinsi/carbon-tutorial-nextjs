dd9b9439ea4508364fcbe834b961452a441e17b6
simeiqin <simeiqin@cn.ibm.com>
2023-11-18 16:19:43 +0800

    feat(tutorial): complete step 1

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,17 @@
+'use client';
+import { Button } from '@carbon/react';
+// export default function Home() {
+//   return (
+//     <div>
+// Hello Carbon! Well, not quite yet. This is the starting point for the Carbon NextJS tutorial.
+// <Button>Button</Button>
+//     </div>
+//
+//   )
+// }
 
-export default function Home() {
-  return (
-    <div>
-Hello Carbon! Well, not quite yet. This is the starting point for the Carbon NextJS tutorial.
-    </div>
+import LandingPage from './home/page';
 
-  )
+export default function Page() {
+  return <LandingPage />;
 }
---
+++ src/components/TutorialHeader/TutorialHeader.js
@@ -0,0 +1,56 @@
+import {
+  Header,
+  HeaderContainer,
+  HeaderName,
+  HeaderNavigation,
+  HeaderMenuButton,
+  HeaderMenuItem,
+  HeaderGlobalBar,
+  HeaderGlobalAction,
+  SkipToContent,
+  SideNav,
+  SideNavItems,
+  HeaderSideNavItems,
+} from '@carbon/react';
+import { Switcher, Notification, UserAvatar } from '@carbon/icons-react';
+
+import Link from 'next/link';
+
+const TutorialHeader = () => (
+  <HeaderContainer
+    render={({ isSideNavExpanded, onClickSideNavExpand }) => (
+      <Header aria-label="Carbon Tutorial">
+        <SkipToContent />
+        <HeaderMenuButton
+          aria-label="Open menu"
+          onClick={onClickSideNavExpand}
+          isActive={isSideNavExpanded}
+        />
+        <Link href="/" passHref legacyBehavior>
+          <HeaderName prefix="IBM">Carbon Tutorial</HeaderName>
+        </Link>
+        <HeaderNavigation aria-label="Carbon Tutorial">
+          <Link href="/repos" passHref legacyBehavior>
+            <HeaderMenuItem>Repositories</HeaderMenuItem>
+          </Link>
+        </HeaderNavigation>
+        <SideNav
+          aria-label="Side navigation"
+          expanded={isSideNavExpanded}
+          isPersistent={false}
+        >
+          <SideNavItems>
+            <HeaderSideNavItems>
+              <Link href="/repos" passHref legacyBehavior>
+                <HeaderMenuItem>Repositories</HeaderMenuItem>
+              </Link>
+            </HeaderSideNavItems>
+          </SideNavItems>
+        </SideNav>
+        <HeaderGlobalBar />
+      </Header>
+    )}
+  />
+);
+
+export default TutorialHeader;
